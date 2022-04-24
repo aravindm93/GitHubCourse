a9d78af4380ae2e0e34dd3915f1ee0907dcf8449
aravindm93 <m_arvind93@yahoo.com>
2022-04-24 22:24:19 +0530

    added/updated/deleted files

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -4,6 +4,9 @@ public class LoginPage {
 
    public static void main(String [] args) {
        System.out.println("Hello Login Page");
+       String un = "admin";
+       String pwd = "test123";
+       System.out.println("User logged in ");
    }
 
 }
