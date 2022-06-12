8b9023cb7170b1c592f362a5cbc762b3a1ed4544
aravindm93 <m_arvind93@yahoo.com>
2022-06-12 23:06:56 +0530

    c1

--- src/Pages/RegPage.java
+++ src/Pages/RegPage.java
@@ -3,6 +3,7 @@ package Pages;
 public class RegPage {
     int i =10;
     int age = 28;
+    String name = "aravind";
 
     public static void main(String[] args) {
         System.out.println("Reg successful");
