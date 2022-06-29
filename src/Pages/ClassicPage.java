feb4818c0474994ccc8170fac1b7d7f013792baa
Aravind M <60823438+aravindm93@users.noreply.github.com>
2022-06-29 22:48:03 +0530

    Create ClassicPage.java

---
+++ src/Pages/ClassicPage.java
@@ -0,0 +1,9 @@
+package Pages;
+
+public class ClassicPage {
+    public void classic() {
+        int x = 100;
+        int y = 200;
+        int sub = y - x;
+        System.out.println(sub);
+    }}
