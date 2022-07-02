6b5995db54697ca606a085d66ff7adc4d1e1548e
aravindm93 <m_arvind93@yahoo.com>
2022-07-02 23:00:17 +0530

    final cart

--- src/Pages/CartPage.java
+++ src/Pages/CartPage.java
@@ -4,8 +4,7 @@ public class CartPage {
 
     public void cartPageTitle() {
         System.out.println("Flipkart");
-        System.out.println("Test1");
-        System.out.println("Test 2");
-        System.out.println("Test 3");
+
     }
+
 }
