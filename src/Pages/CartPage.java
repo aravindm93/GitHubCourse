683fafe92b59a44c65ccec6d904d18d1e086650b
aravindm93 <m_arvind93@yahoo.com>
2022-07-02 22:56:19 +0530

    Cart3

--- src/Pages/CartPage.java
+++ src/Pages/CartPage.java
@@ -6,5 +6,6 @@ public class CartPage {
         System.out.println("Flipkart");
         System.out.println("Test1");
         System.out.println("Test 2");
+        System.out.println("Test 3");
     }
 }
